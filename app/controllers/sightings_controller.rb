d860d00a26b8aa165b6209e9846f3a3560e18035
Brendan Benson <brendanbenson@gmail.com>
2013-01-30 20:51:20 -0700

    blah

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -2,7 +2,7 @@ class SightingsController < ApplicationController
   # GET /sightings
   # GET /sightings.json
   def index
-    @sightings = Sighting.limit(10).all
+    @sightings = Sighting.limit(100).all
 
     respond_to do |format|
       format.html # index.html.erb
